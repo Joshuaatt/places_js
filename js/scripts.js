b0fad2b5aadb570d7f77d5ecf9c82c4172beed46
Guest User <student@epicodus.com>
2015-02-16 10:36:56 -0800

    show place details when clicked

--- js/scripts.js
+++ js/scripts.js
@@ -9,6 +9,12 @@ $(function() {
 
     $("ul#places").append("<li><span class='place'>" + newPlace.location + "</span></li>");
 
-    $(".place")
+    $(".place").last().click(function() {
+      $("#show-place").show();
+      $("#show-place h2").text(newPlace.location); // adds location name to h2 tag
+      $(".location").text(newPlace.location);
+      $(".time-of-year").text(newPlace.timeOfYear);
+      $(".notes").text(newPlace.notes);
+    })
   })
 });
