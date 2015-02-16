adf731e482627cb0862526db4da52bc01b82f459
Guest User <student@epicodus.com>
2015-02-16 10:21:00 -0800

    add js for adding places and their attributes in the form

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,14 @@
 $(function() {
-  
+  $("form#new-place").submit(function(event) {
+    event.preventDefault();
+
+    var inputtedLocation = $("input#location").val();
+    var inputtedTimeOfYear = $("input#time-of-year").val();
+    var inputtedNotes = $("input#notes").val();
+    var newPlace = { location: inputtedLocation, timeOfYear: inputtedTimeOfYear, notes: inputtedNotes };
+
+    $("ul#places").append("<li><span class='place'>" + newPlace.location + "</span></li>");
+
+    $(".place")
+  })
 });
